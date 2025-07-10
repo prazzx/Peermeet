023c30c6c097237ffcb888923c8676f401dd575d
prazzx <prajwalyadavofficial@gmail.com>
2025-07-10 20:28:38 +0545

    nav bar completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,19 @@
+ import logo from '../Assets/logo.png'
 export default function Navbar(){
   return(
-  <div classname= "bg-amber-500 text-5xl">
-<p>
-  nestingngn
-</p>
-  </div>
+    <nav className='text-bg bg-indigo-500 '>
+    <div className='relative max-w-5xl h-15 mx-auto flex items-center justify-between'>
+      <a href='' className="py-1 px-1">
+        <img src = {logo} alt='logo' className= " w-35 h-32" ></img>
+</a>
+<ul className=" flex space-x-8">
+  <li className="text-white px-4 py-1.5 font-mono  cursor-pointer hover:text-black ">About</li>
+  <button className="text-white  px-4 py-1.5 font-mono   cursor-pointer  hover:text-black  border-2 border-solid  border-blue-900 rounded-sm ">Login</button>
+
+  <button className="text-white px-3.5 py-1.5 font-mono  cursor-pointer  hover:text-black  border-2 border-solid  border-blue-900 rounded-sm ">Register now?</button>
+  
+</ul>
+</div>
+  </nav>
   )
 }
