675a4d3cd6477b3f5b1cbf3cce9d7dd4cb1a91d1
prazzx <prajwalyadavofficial@gmail.com>
2025-07-12 12:21:35 +0545

    works till now

--- src/Components/mid.jsx
+++ src/Components/mid.jsx
@@ -1,17 +1,19 @@
+import  hero from '../Assets/herosection.png'
 export default function Body(){
     return(
         <section className="relative bg-sky-600"> 
-                <div>  
+                <div className='flex '>  
                     {/* text wala div  */}
                     <div>
-<h1>Welcome to Peermeet</h1>
+<h1 className='text-sanserif'>Welcome to Peermeet</h1>
 <p>elcome to Peermeetel come to Peerme etelcome to Peermee elcome to Peermeet t</p>
 <div></div>
-<button> sign up now</button>
+  <button className="text-black bg-white hover:bg-white px-3.5 py-1.5 font-mono  cursor-pointer  hover:text-red-400 border-2 border-solid border-blue-900 rounded-sm ">Register now!</button>
+
                     </div>
 {/* pic wala div */}
 <div>
-<img src="" alt="" />
+<img src = {hero} alt= 'hero image' className='mx-auto'  ></img>
 </div>
                 </div>
         </section>
