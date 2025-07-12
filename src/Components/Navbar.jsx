2b1a13b2e72dafbe94198add107ab2e6032c66dc
prazzx <prajwalyadavofficial@gmail.com>
2025-07-12 20:10:56 +0545

    main section done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,8 +7,10 @@ export default function Navbar(){
         <img src = {logo} alt='logo' className= " w-35 h-32" ></img>
 </a>
 <ul className=" flex space-x-8">
-  <li className="text-black px-4 py-1.5 font-mono  font-extrabold cursor-pointer hover:text-teal-800 ">About</li>
-  <li className="text-black px-4 py-1.5 font-mono  font-extrabold cursor-pointer hover:text-teal-800 ">Features</li>
+
+  <a href='#' className="text-black px-4 py-1.5 font-mono  font-extrabold cursor-pointer hover:text-teal-800 ">About</a>
+  <a  href='#features' className="text-black px-4 py-1.5 font-mono  font-extrabold cursor-pointer hover:text-teal-800">Features</a>
+
   <li className="text-black font-extrabold    hover:text-sky-900 hover:bg-white  px-4 py-1.5 font-mono   cursor-pointer  ">Login</li>
 
   <button className="text-white bg-indigo-600 hover:bg-white px-3.5 py-1.5 font-mono  cursor-pointer  hover:text-black  border-2 border-solid border-blue-900 rounded-sm ">Register now?</button>
