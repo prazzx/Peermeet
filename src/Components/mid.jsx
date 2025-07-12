bff28ec98091d072fcd0ee48a97cca2b8f79f3fc
prazzx <prajwalyadavofficial@gmail.com>
2025-07-12 21:18:42 +0545

    landing pg done

--- src/Components/mid.jsx
+++ src/Components/mid.jsx
@@ -1,9 +1,10 @@
 import  hero from '../Assets/herosection.png'
 import hero2 from '../Assets/herosection2.png'
+import logo  from '../Assets/logo.png'
 export default function Body(){
     return(
-        <div>
-        <section className="relative bg-white "> 
+        <div className='' >
+        <section className="relative  "> 
                   
               {/*   main div  */}
 
@@ -49,6 +50,17 @@ Our AI recommends the best matches for your goals.</li>
 </div>
 
 </section>
+<footer className='bg-gray-500 text-gray-100 '>
+    <div className='grid grid-cols-3 gap-3 mx-auto py-12 px-6 '>
+
+        <img src = {logo} alt='logo' className= " w-40 h-40" ></img>
+        <p  className='col-span-3'>Connecting Students, Building Futures</p>
+        <p>© 2025 PeerMeet. All rights reserved.</p>
+   
+
+    </div>
+
+</footer>
 
         </div>
     )
