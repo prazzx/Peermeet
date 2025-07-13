cfa2187d2cb4a0558a563965a138ea540c3123ca
AmanKarn-00 <karn.aman2007@gmail.com>
2025-07-13 23:09:29 +0545

    Added body parser and cors to the server

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 require('./Models/db');
 
@@ -8,6 +10,9 @@ const PORT = process.env.PORT;
 app.get("/", (req, res) => {
   res.send("Backend is working!");
 });
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
