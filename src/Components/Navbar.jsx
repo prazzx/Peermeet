abd8e1ba2d3d9f54528c90c5891244ee8004901a
prazzx <prajwalyadavofficial@gmail.com>
2025-07-13 22:35:26 +0545

    this much for todaay

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,17 +5,24 @@ export default function Navbar(){
   return(
     <nav className='text-bg border-b-2 '>
     <div className='relative max-w-5xl h-15 mx-auto flex items-center justify-between'>
-      <a href='' className="py-1 px-1">
+      <NavLink to = '' className="py-1 px-1">
         <img src = {logo} alt='logo' className= " w-35 h-32" ></img>
-</a>
+</NavLink>
 <ul className=" flex space-x-8">
 
-  <a href='#' className="text-black px-4 py-1.5 font-mono  font-extrabold cursor-pointer hover:text-teal-800 ">About</a>
-  <a  href='#features' className="text-black px-4 py-1.5 font-mono  font-extrabold cursor-pointer hover:text-teal-800">Features</a>
+  <NavLink to='/about' className="text-black px-4 py-1.5 font-mono  font-extrabold cursor-pointer hover:text-teal-800 ">About</NavLink>
+  
+ <a href='/' className="text-black px-4 py-1.5 font-mono font-extrabold cursor-pointer hover:text-teal-800" onClick={(e) => {
+  e.preventDefault();
+  window.location.href = '/#features';
+}}>
+  Features
+</a>
 
-  <li className="text-black font-extrabold    hover:text-sky-900 hover:bg-white  px-4 py-1.5 font-mono   cursor-pointer  ">Login</li>
 
-  <button className="text-white bg-indigo-600 hover:bg-white px-3.5 py-1.5 font-mono  cursor-pointer  hover:text-black  border-2 border-solid border-blue-900 rounded-sm ">Register now?</button>
+  <NavLink  to = '/login'className="text-black font-extrabold    hover:text-sky-900 hover:bg-white  px-4 py-1.5 font-mono   cursor-pointer  ">Login</NavLink>
+
+  <NavLink to = '/signup' className="text-white bg-indigo-600 hover:bg-white px-3.5 py-1.5 font-mono  cursor-pointer  hover:text-black  border-2 border-solid border-blue-900 rounded-sm ">Register now?</NavLink>
   
 </ul>
 </div>
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-     login page 
-    </div>
+<>
+
+</>
+
   )
 }
 
