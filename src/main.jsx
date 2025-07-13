1b1830b424fe0feea16eda46dfea021828957435
prazzx <prajwalyadavofficial@gmail.com>
2025-07-13 22:35:26 +0545

    this much for todaay

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./index.css"
-import Body from './mid'
+import Body from './body'
 import Root from './Root'
 import About from './Pages/About'
+import Login from './Pages/Login'
 import { createBrowserRouter , RouterProvider } from 'react-router'
+import Signup from './Pages/Signup'
 
 const routing101 = createBrowserRouter([
   {
     path: '/'
     , element: <Root/>,
     children: [
-{ path:"", element:<Body/>} , { path:"About",element:<About/>}
+{ path:"", element:<Body/>} , { path:"About",element:<About/>}, { path:"login",element:<Login/>} , { path:"signup",element:<Signup/>}
 
     ]  },
 ])
