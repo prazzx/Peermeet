3a305c08502010803dcfdbb92c3b111733908178
AmanKarn-00 <karn.aman2007@gmail.com>
2025-07-14 00:27:23 +0545

    Added validation for login and signup

---
+++ Backend/Controllers/AuthControllers.js
@@ -0,0 +1,32 @@
+const bcrypt = require('bcrypt');
+const Usermodel = require("../Models/Users");
+const Usermodel = require("../Models/Users");
+
+const signup = async (req , res) =>{
+    try{
+        const {name, email, password} = req.body;
+        const user = await Usermodel.findOne({email});
+        if(user){
+            return res.status(409)
+            .json({message: "User already exists", success:false});
+        }
+        const Usermodel = new Usermodel({name, email, password});
+        Usermodel.password = await bcrypt.hash(password, 10);
+        await Usermodel.save();
+        res.status(201)
+        .json({message: "Signup Successful",
+            success:true
+        })
+    }
+    catch(err){
+        return res.status(500)
+            .json({message: "Internal Error", 
+                success:false});
+    }
+
+}
+
+
+module.exports = {
+    signup
+}
