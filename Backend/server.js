804256f129c3bc82288989e0d21e4fc553bd20a8
AmanKarn-00 <karn.aman2007@gmail.com>
2025-07-16 15:01:42 +0545

    Connecting google auth to backend

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const admin = require("./firebase");
 require('dotenv').config();
 require('./Models/db');
 const Router = require('./Routes/AuthRouter');
@@ -15,6 +16,35 @@ app.get("/api", (req, res) => {
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/auth', Router);
+
+async function verifyToken(req, res, next) {
+  const idToken = req.headers.authorization;
+
+  if (!idToken) {
+    return res.status(401).send("Unauthorized");
+  }
+
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(idToken);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(401).send("Unauthorized");
+  }
+}
+
+app.post("/api/protected", verifyToken, async (req, res) => {
+  const { name, email} = req.user;
+
+  let user = await User.findOne({ uid });
+
+  if (!user) {
+    user = new User({  name, email});
+    await user.save();
+  }
+
+  res.send(user);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
