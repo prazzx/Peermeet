3876fcf42e071c15e61fc74519f28d3748d701e1
prazzx <prajwalyadavofficial@gmail.com>
2025-07-19 10:24:40 +0545

    reverted navbar animation

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
-import logo from '../Assets/logo.png'
-import {Link,NavLink} from 'react-router-dom'
+ import logo from '../Assets/logo.png'
+ import {Link,NavLink} from 'react-router-dom'
 
 export default function Navbar(){
   return(
@@ -10,18 +10,19 @@ export default function Navbar(){
 </NavLink>
 <ul className=" flex space-x-8">
 
-  <NavLink to='/about' className="text-black px-4 py-1.5 font-mono font-extrabold cursor-pointer hover:text-teal-800 hover:bg-teal-50 rounded-md transition-all duration-300 hover:scale-105">About</NavLink>
+  <NavLink to='/about' className="text-black px-4 py-1.5 font-mono  font-extrabold cursor-pointer hover:text-teal-800 ">About</NavLink>
   
- <a href='/' className="text-black px-4 py-1.5 font-mono font-extrabold cursor-pointer hover:text-teal-800 hover:bg-teal-50 rounded-md transition-all duration-300 hover:scale-105"  onClick={(e) => {
+ <a href='/' className="text-black px-4 py-1.5 font-mono font-extrabold cursor-pointer hover:text-teal-800" onClick={(e) => {
   e.preventDefault();
   window.location.href = '/#features';
 }}>
   Features
 </a>
 
-  <NavLink  to = '/login'className="text-black font-extrabold hover:text-sky-900 hover:bg-sky-50 px-4 py-1.5 font-mono cursor-pointer rounded-md transition-all duration-300 hover:scale-105">Login</NavLink>
 
-  <NavLink to = '/signup' className="text-white bg-indigo-600 hover:bg-indigo-700 px-3.5 py-1.5 font-mono cursor-pointer hover:text-white border-2 border-solid border-indigo-600 hover:border-indigo-700 rounded-sm transition-all duration-300 hover:scale-105 hover:shadow-lg">Register now?</NavLink>
+  <NavLink  to = '/login'className="text-black font-extrabold    hover:text-sky-900 hover:bg-white  px-4 py-1.5 font-mono   cursor-pointer  ">Login</NavLink>
+
+  <NavLink to = '/signup' className="text-white bg-indigo-600 hover:bg-white px-3.5 py-1.5 font-mono  cursor-pointer  hover:text-black  border-2 border-solid border-blue-900 rounded-sm ">Register now?</NavLink>
   
 </ul>
 </div>
